9d4c736d07b2a00514688abbfd5fef6967a3035c
Peter Kreppein <pmkreppein@gmail.com>
2018-10-09 22:06:26 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,6 +16,6 @@ class String
 
   def count_sentences
    
-  self.split(/["']?[A-Z][^.?!]+((?![.?!]['"]?\s["']?[A-Z][^.?!]).)+[.?!'"]+/).delete_if {|w| w.size < 2}.size
+  self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
   end
 end
